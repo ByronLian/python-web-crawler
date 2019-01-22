68760aa10591787e30d67fdfb8b53ca8b862857c
Byron <gordon.byron.lien@gmail.com>
2019-01-22 10:37:24 +0800

    Fix code

--- main.py
+++ main.py
@@ -2,37 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 
 # This function is used to get entire page from specific url
-def get_page(url):
+def get_html(url):
   # https://www.google.com/search?q=my+user+agent Find user agent
   headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
     
   # http://docs.python-requests.org/en/master/user/quickstart/#make-a-request
-  result = requests.get(url, headers=headers)
-  return result.text
+  content = requests.get(url, headers = headers)
+  return content.text
 
 
 # This function is used to process stock info
-def get_page_content(html, stock_no):
-    # Final result format
-    final_result = "Stock Name: {}\n Bid Price: {}\n Change{}\n\n\n"
+def get_result_from_html(html, stock_no):
     html_content = BeautifulSoup(html, 'html.parser')
     
     # Processing HTML DOM data
     name = html_content.find('h3', class_='idx-name').get_text()
-    bid_price = html_content.find('span', class_='price up').get_text()
+    bid_price = html_content.find('span', class_='price').get_text()
     change = html_content.find('span', class_='chg').get_text()
 
     # Save result
+    final_result = "Stock Name: {}\n Bid Price: {}\n Change{}\n\n\n"
     save_final_result(final_result.format(name, bid_price, change))
 
 
 # This function is used to save result to txt file
 # Note: will change to Database later
 def save_final_result(*args):
-  for i in args:
-    with open('stock_info.txt', 'a', encoding='utf-8') as file:
-      file.write(i)
-
+  try:
+      for i in args:
+        with open('stock_info.txt', 'a', encoding='utf-8') as file:
+          file.write(i)
+  except:
+      print('something wrong')
+  finally:
+      file.close()
 
 def main():
   stock_ary = ['0050', '2330'] # Stock serial number
@@ -40,8 +43,8 @@ def main():
 
   for i in range(0, stock_ary_length):
     url = 'https://www.wantgoo.com/stock/' + stock_ary[i]
-    html = get_page(url)
-    get_page_content(html, stock_ary[i])
+    html = get_html(url)
+    get_result_from_html(html, stock_ary[i])
 
 
 if __name__ == '__main__':
